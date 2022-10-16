3b2b3a41f21c17de8ddfdecf819e016f23de2be9
Kefka-esque <linkbabineau@gmail.com>
2022-10-15 20:50:19 -0400

    Tile and mapgen
    tile class added, mapgen added to create an array of tiles. Seems to work.

--- colours.py
+++ colours.py
@@ -1,4 +1,4 @@
-# this is a seperate module, so that classes and functions can use
+# this is a seperate module so that classes and functions can use
 # them without haing to messily declare them at the top of the file
 # use c.[colour] in the main py file.
 
@@ -7,3 +7,9 @@ white = (255, 255, 255)
 green = (0, 255, 0)
 blue = (0, 0, 128)
 red = (255, 0, 0)
+darkRed = (139,0,0)
+darkGrey = (105,105,105)
+lightGrey = (211,211,211)
+purple = (128,0,128)
+brown = (139,69,19)
+tan = (210,180,140)
--- deadhearth.py
+++ deadhearth.py
@@ -1,9 +1,8 @@
 import pygame
 import os
 from pygame import font
+
 from pygame.locals import( 
-    # more to be added as needed
-    # also, do we actually need this? w and s are used below and work, even thought they aren't imported here...
     K_UP,
     K_DOWN,
     K_LEFT,
@@ -12,9 +11,11 @@ from pygame.locals import(
     K_RETURN,
     KEYDOWN,
     QUIT,
-)
+) 
+
 import colours as c # use c.[colour], right now only very basic colours supported
 
+# for text
 class textItem:
     colour = ''
     content = ''
@@ -40,8 +41,125 @@ class textItem:
 
     def display(self, x, y): # render to screen
         screen.blit(self.itself, (x, y))
-    
 
+# tiles that go in the map array
+class tile: 
+	type = '' # str for tile name
+	symbol = '' # symbol to display on map
+	moveable = True # can this tile be moved through?
+	transparent = True
+	
+	def __init__(this, type):
+		this.type = type
+		if type == 'grass':
+			this.symbol = '"'
+			this.colour = c.green
+		
+		if type == 'floor':
+			this.symbol = '.'
+			this.colour = c.white
+		
+		if type == 'vWall':
+			this.symbol = 'ǁ'
+			this.colour = c.brown
+			this.moveable = False
+			this.transparent = False
+
+		if type == 'hWall':
+			this.symbol = '='
+			this.colour = c.brown
+			this.moveable = False
+			this.transparent = False
+
+		if type == 'cWall':
+			this.symbol = '^'
+			this.colour = c.brown
+			this.moveable = False
+			this.transparent = False
+
+		if type == 'door':
+			this.symbol = '∩'
+			this.colour = c.tan
+			this.moveable = True
+			this.transparent = False
+
+		if type == 'water':
+			this.symbol = '~'
+			this.colour = c.blue
+			this.moveable = False
+
+		if type == 'bridge':
+			this.symbol = '#'
+			this.colour = c.brown
+			
+			
+	
+	# return the tile type
+	def getType(this): 
+		return this.type
+
+def generateMap():
+    
+    map = [[]]
+    i = 0
+    
+    for i  in range(32):
+        
+        for o in range(32):
+            map[i].append(tile('grass'))
+
+        if i < 31:
+            map.append([])
+    return map
+
+
+def menu():
+    titleText = textItem(c.white, 'DEADHEARTH', 'courbd.ttf', 96)
+    menuOptions = [textItem(c.white, 'START GAME', 'cour.ttf', 40), textItem(c.white, 'QUIT', 'cour.ttf',40)]  
+    menuOptions[0].select()
+    menu = True
+    # loop for the main menu:
+    while menu:
+        screen.fill(c.black)
+        titleText.display(150, 200)
+        menuOptions[0].display(200, 300)
+        menuOptions[1].display(200, 345)
+        pygame.display.flip()
+        for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # User clicks the X
+                menu = False
+            if event.type == KEYDOWN:
+                # quit on escape key
+                if event.key == pygame.K_ESCAPE: 
+                    menu = False
+                # move down the menu
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    for i in range(len(menuOptions)): # maybe make this function a part of textItem class?
+                        if menuOptions[i].selected and i != (len(menuOptions)) - 1:
+                            menuOptions[i].unselect()
+                            menuOptions[i + 1].select()
+                            break
+                
+                # move up the menu
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    for i in range(len(menuOptions)): # maybe make this function a part of textItem class?
+                        if menuOptions[i].selected and i != 0:
+                            menuOptions[i].unselect()
+                            menuOptions[i - 1].select()
+                            break
+
+                # make a menu selection
+                if event.key == pygame.K_SPACE or event.key == K_RETURN:
+                    # if start game selected:
+                    if menuOptions[0].selected:
+                        running = True
+                        menu = False
+                        return running
+                        break
+
+                    if menuOptions[1].selected:
+                        menu = False
+                        break
 # let pygame do its thing
 pygame.init()
 
@@ -53,59 +171,18 @@ pygame.display.set_caption("TEST")
 # define some colours 
 # MOVED to colours.py so that colours can be used in class functions
 
-# bools for the game and menu loops 
-menu = True
-running = False
-
 # for proper FPS
 clock = pygame.time.Clock()
 
-# globals... yuck! Look into a better way to do this!
-titleText = textItem(c.white, 'DEADHEARTH', 'courbd.ttf', 96)
-menuOptions = [textItem(c.white, 'START GAME', 'cour.ttf', 40), textItem(c.white, 'QUIT', 'cour.ttf',40)]  
-menuOptions[0].select()
+running = menu() # run the main menu and return a bool for the main loop
 
-# loop for the main menu:
-while menu:
-    screen.fill(c.black)
-    titleText.display(150, 200)
-    menuOptions[0].display(200, 300)
-    menuOptions[1].display(200, 345)
-    pygame.display.flip()
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # User clicks the X
-            menu = False
-        if event.type == KEYDOWN:
-            # quit on escape key
-            if event.key == pygame.K_ESCAPE: 
-                menu = False
-            # move down the menu
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                for i in range(len(menuOptions)): # maybe make this function a part of textItem class?
-                    if menuOptions[i].selected and i != (len(menuOptions)) - 1:
-                        menuOptions[i].unselect()
-                        menuOptions[i + 1].select()
-                        break
-            # move up the menu
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
-                for i in range(len(menuOptions)): # maybe make this function a part of textItem class?
-                    if menuOptions[i].selected and i != 0:
-                        menuOptions[i].unselect()
-                        menuOptions[i - 1].select()
-                        break
+map = generateMap()
 
-            # make a menu selection
-            if event.key == pygame.K_SPACE or event.key == K_RETURN:
-                # if start game selected:
-                if menuOptions[0].selected:
-                    running = True
-                    menu = False
-                    break
+for i in range(32):
+    for o in range(32):
+        print(map[i][o].colour)
+print(len(map))
 
-                if menuOptions[1].selected:
-                    menu = False
-                    break
-            
 while running: # loop for the main game
     clock.tick(60) # set to 60 fps
     screen.fill(c.white)
